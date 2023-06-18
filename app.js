6c871bd6dae64cf959af2f2fdbcf18ce1aecc50e
Anjan Prasad <anjanprasad112@gmail.com>
2023-06-18 21:52:17 +0530

    2/2

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ const bodyParser = require('body-parser');
 
 
 app.use(express.json());
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 
 
