7ccbe26d54945b1b0edd7aab61608c433fc7a69c
Anjan Prasad <anjanprasad112@gmail.com>
2023-06-18 22:02:11 +0530

    3/3

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions))
+
+app.use(cors(
+  {
+    origin: 'https://rescue-task-client.vercel.app/',
+  }
+))
 
 
 
